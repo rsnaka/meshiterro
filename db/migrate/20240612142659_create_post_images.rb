ef4cf546f76a45179fae34732c76f9aa8a6ad8d2
rsnaka <166614766+rsnaka@users.noreply.github.com>
2024-06-13 14:55:48 +0000

    [start]応用1-13

--- db/migrate/20240612142659_create_post_images.rb
+++ db/migrate/20240612142659_create_post_images.rb
@@ -1,7 +1,6 @@
-class CreatePostlmages < ActiveRecord::Migration[6.1]
+class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
-    create_table :postlmages do |t|
-      t.integer :id
+    create_table :post_images do |t|
       t.string :shop_name
       t.text :caption
       t.integer :user_id
