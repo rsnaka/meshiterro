c0f7b3d68351455243b110e06db92f3833331519
rsnaka <166614766+rsnaka@users.noreply.github.com>
2024-06-16 15:00:08 +0000

    [start]応用1-18コメント投稿機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to '/users'
+    redirect_to user_path(@user)
   end
 
 
